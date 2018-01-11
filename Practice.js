f3cd7c14fb42c99771dca7343f5e8ac24a79bbc9
mohitkumaris <mohitkumaris@gmail.com>
2018-01-11 15:43:31 +0530

    Prototype using classes

--- Practice.js
+++ Practice.js
@@ -79,12 +79,65 @@ every object that is created by that function as constructor.
 
 An Object's prototype is the object instance from
 which the object is inherited.
+
+__proto__ is an instance of object created by
+ function object.
+
+
  */
 
 var cat ={name:'billu',age:12};
 
 //Object.defineProperty(cat,'name',{configurable:false})
 //Object.freeze(cat);
-console.log(cat);
-cat.name='pussy';
-console.log(cat);
+//console.log(cat);
+//cat.name='pussy';
+//console.log(cat.__proto__);
+
+function dog(name,color) {
+
+    this.name=name;
+    this.color=color
+}
+
+var dog1= new dog("crazy","golden");
+
+dog.prototype.age=3;
+
+
+dog.prototype ={sex:'male'}; // new objects in memory
+
+var dog2= new dog("bhola","ghehua");
+console.log(dog1.__proto__.age);
+console.log(dog2.__proto__.sex);
+
+
+/*
+ Creating own Prototype using class
+*/
+
+class Animal{
+    constructor(voice){
+
+        this.voice=voice || 'grunt';
+    }
+
+    Speak(){
+
+        console.log(this.voice);
+    }
+
+}
+
+class Cat extends Animal{
+    constructor(name,color){
+
+        super("meow"); // calls parent class constructor
+        this.name=name;
+        this.color=color;
+    }
+
+}
+
+var cat1= new Cat('joe','white');
+cat1.Speak();
