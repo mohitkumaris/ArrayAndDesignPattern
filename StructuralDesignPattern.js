2a22e33bfa5778908cd15da7dca27a1fa2102185
mohitkumaris <mohitkumaris@gmail.com>
2018-01-13 19:33:21 +0530

    decorator

--- StructuralDesignPattern.js
+++ StructuralDesignPattern.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 /*
 Decorator pattern
@@ -6,3 +6,23 @@ used to add new functionality  to an existing object
 without being obstructive.
 */
 
+var demodecorator=function (name,age) {
+
+    this.name=name;
+    this.age=age;
+
+
+    demodecorator.prototype.showData=function () {
+
+        console.log(name + ' ' + age);
+    }
+};
+
+var decoobject= new demodecorator('mohit',33);
+decoobject.showData();
+
+decoobject.shownewData=function () {
+
+    console.log('new data without changing the first one');
+};
+decoobject.shownewData();
