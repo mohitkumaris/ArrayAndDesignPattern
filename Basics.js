791a28583b37002bf0f3647479df3cb2ef4aaff0
mohitkumaris <mohitkumaris@gmail.com>
2018-07-31 13:33:02 +0530

    this basics

--- Basics.js
+++ Basics.js
@@ -1,12 +1,30 @@
 "use strict"
-console.log(this === global);
+//console.log(this === global);
 
 const hero={
   HeroName:"Batman",
   dialogue(){
   console.log(`I am ${this.HeroName}`);
 }
-}
+};
 
 let print=hero.dialogue.bind(hero);
-print()
+//print();
+
+
+const posts = [
+  {id: 1, title: 'Title 1'},
+  {id: 2, title: 'Title 2'}
+];
+const updatedPosts = posts.map(p => p.id !== 1 ? p : {...p, title: 'Updated Title 1'}
+);
+
+console.log(updatedPosts);
+
+/*
+updatedPosts is now
+[
+  {id: 1, title: 'Updated Title 1'},
+  {id: 2, title: 'Title 2'}
+];
+*/
